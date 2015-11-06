9a0a7b34c948f0aa4ea9f9ffbd3d137a6a0a6602
snehal singh <snehal@idyllic-software.com>
2015-11-06 11:20:03 +0530

    added angular js app

---
+++ Angular_Js_app/README.md
@@ -0,0 +1,2 @@
+# Angular_app
+Sample Agulare application
---
+++ Angular_Js_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularApp::Application.config.secret_token = '981b7e6d6625d6553ae171ef9f66b6110efdefb20592e9c1697291113f026bbcfd99849273896f2dcdc9aeeab3d9fa720fb5f108495d51566f0f9cd4ab8684d5'
